a7228db14f9adaab3ca0d48fb7c902184a3b7f8a
Adrian.B <Adrian_bc@aol.com>
2022-05-29 23:52:17 -0500

    nav bar and hero page

---
+++ src/components/Hero.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Hero.css'
+
+function Hero() {
+    return (
+        <div className='Hero-container'>
+            <div className='Hero-style'>
+                <h1> Hi I`m Adrian C Butler</h1>
+                <h1> A Frontend developer and UX & UI Designer</h1>
+            </div>
+
+            <div className='Hero-image'>
+
+            </div>
+        </div>
+
+    )
+}
+
+export default Hero
